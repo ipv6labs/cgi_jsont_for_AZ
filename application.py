1fd2e16e89e180e2d2cd87f430585117a9990a12
ipv6labs <54894903+ipv6labs@users.noreply.github.com>
2019-09-05 08:55:28 +0900

    Update application.py

--- application.py
+++ application.py
@@ -47,6 +47,5 @@ def exec_json():
 
 @app.route("/cgi-bin/jsont")
 def jsont():
-    result_jsont = 'result_jsont'
-    #resalt_jsont = print ('jsont( {\n} )')
+    resalt_jsont = print ('jsont( {\n} )')
     return result_jsont
