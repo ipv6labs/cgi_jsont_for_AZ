407da0b567afed624a1dc7eab4db2e0a4aa0dd44
ipv6labs <54894903+ipv6labs@users.noreply.github.com>
2019-09-05 05:30:45 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+import time
+import os
+import platform
+
+from datetime import datetime
+from socket import gethostname
+
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
 @app.route("/jsont")
 def jsont():
-    return "jsont run"
+    #return "jsont run"
+    print(' "pf": "'+platform.system()+' '+platform.machine()+'",')
