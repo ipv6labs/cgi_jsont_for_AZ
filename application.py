d36b819e0608a36e25392cd68affcf144b04731b
ipv6labs <54894903+ipv6labs@users.noreply.github.com>
2019-09-05 07:04:38 +0900

    Update application.py

--- application.py
+++ application.py
@@ -24,9 +24,8 @@ def _exec_ntp():
 def _exec_time():
     return "EXEC time!"
 
-@app.route("/jst")
+@app.route("/cgi-bin/jst")
 def exec_jst():
-    #print_jst = "{0:.3f}".format(time.time())
     return "{0:.3f}".format(time.time())
 
 @app.route("/jsont")
