d4683f519b0085a4509ec011c79724249a2a5d3a
ipv6labs <54894903+ipv6labs@users.noreply.github.com>
2019-09-05 08:09:16 +0900

    Update application.py

--- application.py
+++ application.py
@@ -18,7 +18,7 @@ def hello():
 
 @app.route("/cgi-bin/time")
 def exec_time():
-    result_time = datetime.now()
+    result_time = print(datetime.now())
     return result_time
 
 @app.route("/cgi-bin/jst")
