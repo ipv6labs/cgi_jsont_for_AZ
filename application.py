7ef872c4efc17c045f79271134644fce502496ec
ipv6labs <54894903+ipv6labs@users.noreply.github.com>
2019-09-05 06:12:09 +0900

    Update application.py

--- application.py
+++ application.py
@@ -20,8 +20,9 @@ def hello():
 def jsont():
     _my_pf_sys = print(platform.system()+' '+platform.machine())
     result = {
-      "it": 0,
+      "it": 0.000,
       "pf": _my_pf_sys,
+      "st": "{0:.3f}".format(time.time()),
     "leep": print_leep,
     "next": print_next,
     "step": print_step
