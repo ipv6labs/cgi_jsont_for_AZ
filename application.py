22094a80feca053ffedb9887862e1358bd23cc99
ipv6labs <54894903+ipv6labs@users.noreply.github.com>
2019-09-05 05:16:52 +0900

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,10 @@
-import time
-import os
-import platform
-
-from datetime import datetime
-from socket import gethostname
-
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 
-print(' "id": "'+platform.node()+'",')
+def hello():
+    return "Hello World!"
+
+def hello2():
+    return "Hello World2!"
